479c9695b2a1312f97a6bfa3ae955fb647b68fc4
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-06-26 17:58:51 +0200

    grafic

--- main.js
+++ main.js
@@ -1,1 +1,64 @@
+import Chart from 'chart.js/auto'
 
+(async function() {
+  const data = [
+    { year: 2010, count: 10 },
+    { year: 2011, count: 40 },
+    { year: 2012, count: 15 },
+    { year: 2013, count: 25 },
+    { year: 2014, count: 22 },
+    { year: 2015, count: 30 },
+    { year: 2016, count: 28 },
+  ];
+
+  // new Chart(
+  //   document.getElementById('acquisitions'),
+  //   {
+  //     type: 'bar',
+  //     data: {
+  //       labels: data.map(row => row.year),
+  //       datasets: [
+  //         {
+  //           label: 'Acquisitions by year',
+  //           data: data.map(row => row.count )
+  //         }
+  //       ]
+  //     }
+  //   }
+  // );
+
+
+  new Chart(
+    document.getElementById('acquisitions'),
+    {
+      type: 'line',
+      options: {
+        animation: false,
+        plugins: {
+          legend: {
+            display: false
+          },
+          tooltip: {
+            enabled: false
+          }
+        }
+      },
+      data: {
+        labels: data.map(row => row.year),
+        datasets: [
+          {
+            label: 'Acquisitions by year',
+            data: data.map(row => row.count)
+          }
+        ]
+      }
+    }
+  );
+  
+})();
+
+// window.addEventListener('resize', function(){
+//   camera.aspect = window.innerWidth/window.innerHeight;
+//   camera.updateProjectionMatrix();
+//   renderer.setSize(window.innerWidth,window.innerHeight);
+// });
